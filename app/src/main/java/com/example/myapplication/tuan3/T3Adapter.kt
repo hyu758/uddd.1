9e670b70020b1c1056a452bfd4b10e917b811217
Sinh vien(pass:123456) <huy20041512@gmail.com>
2024-04-10 10:12:53 +0700

    tuan 3

--- app/src/main/java/com/example/myapplication/tuan3/T3Adapter.kt
+++ app/src/main/java/com/example/myapplication/tuan3/T3Adapter.kt
@@ -1,4 +1,56 @@
 package com.example.myapplication.tuan3
 
-class T3Adapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.example.myapplication.R
+
+class T3Adapter(val ls: ArrayList<T3Contact>, val ct: Context): BaseAdapter() {
+    override fun getCount(): Int {
+        return ls.size
+    }
+
+    override fun getItem(position: Int): Any {
+        return ls[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View? {
+        //tao view
+        var cvv = convertView
+        val vax: ViewAx
+        if (cvv == null){
+            vax = ViewAx()
+            cvv = LayoutInflater.from(ct).inflate(R.layout.itemview,  null)
+            vax.img = cvv.findViewById(R.id.t3ItemHinh)
+            vax.name = cvv.findViewById(R.id.t3ItemTen)
+            vax.age = cvv.findViewById(R.id.t3ItemTuoi)
+
+            //tao template
+            cvv.tag = vax
+        }
+        else{
+            vax = cvv.tag as ViewAx
+        }
+        vax.img!!.setImageResource(ls[position].hinh!!)
+        vax.name!!.text = ls[position].ten
+        vax.age!!.text = ls[position].tuoi
+
+        return cvv
+    }
+    //dinh nghia class cua item view
+    internal inner class ViewAx{
+        var img : ImageView? = null;
+        var name : TextView? = null;
+        var age : TextView? = null;
+
+    }
+
 }
--- app/src/main/java/com/example/myapplication/tuan3/T3ListViewMainAc.kt
+++ app/src/main/java/com/example/myapplication/tuan3/T3ListViewMainAc.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication.tuan3
 
 import android.os.Bundle
+import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,6 +9,8 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.myapplication.R
 
 class T3ListViewMainAc : AppCompatActivity() {
+    var adt : T3Adapter ?= null
+    var ls = ArrayList<T3Contact>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,5 +20,17 @@ class T3ListViewMainAc : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        var lv = findViewById<ListView>(R.id.t3ListView);
+        ls.add(T3Contact("Nguyen Van A", "15", R.drawable.android))
+        ls.add(T3Contact("Nguyen Van DELL", "32", R.drawable.dell))
+        ls.add(T3Contact("Lmao Lmao", "17", R.drawable.facebook))
+        ls.add(T3Contact("Bruh Bruh", "20", R.drawable.apple))
+        ls.add(T3Contact("Brrrrr", "25", R.drawable.blogger))
+        ls.add(T3Contact("ZZAXXZZX", "15", R.drawable.chrome))
+        ls.add(T3Contact("Alime", "35", R.drawable.hancock))
+
+        adt = T3Adapter(ls, this)
+        lv!!.adapter = adt
+
     }
 }
